26fd167eef2ef95a46d176eb925d2520dd999710
sariwahyu <sariwahyuningsih22@gmail.com>
2020-04-25 19:59:32 +0700

    reupload scene

---
+++ Assets/Scenes/Script/LoadIndex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadIndex : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+    public void SceneLoaderString(string SceneString)
+    {
+        SceneManager.LoadScene(SceneString);
+    }
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
